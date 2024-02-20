f1d96f12dd09e9aae318d65e4e86693a5b144bd1
pingchenchang <judychang0908@gmail.com>
2024-02-19 20:42:05 -0700

    Lesson1-2

---
+++ Lesson1/favorNumber.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int favoriteNum = 0;
+    printf("Please enter your favorite number:\n ");
+    scanf("%d", &favoriteNum);
+    printf("Your favorite number: %d \n", favoriteNum);
+    return 0;
+}
